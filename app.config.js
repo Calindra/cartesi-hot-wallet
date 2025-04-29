2d6e60fe51771d6ad94881bbc491dea573113589
Leonardo <leonardo.ghiggino@calindra.com.br>
2025-04-29 10:17:08 -0300

    chore: upgraded build version

--- app.config.js
+++ app.config.js
@@ -1,72 +1,72 @@
-const VERSION = '0.0.2'
-const IOS_BUILDNUMBER = '2'
+const VERSION = '0.0.3';
+const IOS_BUILDNUMBER = '3';
 module.exports = {
-  expo: {
-    name: 'Cartesi Hot Wallet',
-    slug: 'cartesi-hot-wallet',
-    version: VERSION,
-    orientation: 'portrait',
-    icon: './assets/images/cartesi-hot-wallet-logo.png',
-    scheme: 'myapp',
-    userInterfaceStyle: 'automatic',
-    newArchEnabled: true,
-    ios: {
-      bundleIdentifier: 'io.cartesi.hotwallet',
-      supportsTablet: false,
-      buildNumber: IOS_BUILDNUMBER,
-    },
-    android: {
-      adaptiveIcon: {
-        foregroundImage: './assets/images/cartesi-hot-wallet-logo.png',
-        backgroundColor: '#ffffff',
-      },
-      versionCode: IOS_BUILDNUMBER,
-      package: 'io.cartesi.hotwallet',
-    },
-    web: {
-      bundler: 'metro',
-      output: 'static',
-      favicon: './assets/images/favicon.png',
-    },
-    plugins: [
-      'expo-router',
-      [
-        'expo-splash-screen',
-        {
-          image: './assets/images/splash.png',
-          imageWidth: 800,
-          resizeMode: 'cover',
-          backgroundColor: '#a435f1',
+    expo: {
+        name: 'Cartesi Hot Wallet',
+        slug: 'cartesi-hot-wallet',
+        version: VERSION,
+        orientation: 'portrait',
+        icon: './assets/images/cartesi-hot-wallet-logo.png',
+        scheme: 'myapp',
+        userInterfaceStyle: 'automatic',
+        newArchEnabled: true,
+        ios: {
+            bundleIdentifier: 'io.cartesi.hotwallet',
+            supportsTablet: false,
+            buildNumber: IOS_BUILDNUMBER,
         },
-      ],
-      [
-        'expo-dev-client',
-        {
-          launchMode: 'most-recent',
+        android: {
+            adaptiveIcon: {
+                foregroundImage: './assets/images/cartesi-hot-wallet-logo.png',
+                backgroundColor: '#ffffff',
+            },
+            versionCode: IOS_BUILDNUMBER,
+            package: 'io.cartesi.hotwallet',
         },
-      ],
-      [
-        '@sentry/react-native/expo',
-        {
-          organization: 'calindra',
-          project: 'cartesi-hot-wallet',
-          url: 'https://sentry.io/',
+        web: {
+            bundler: 'metro',
+            output: 'static',
+            favicon: './assets/images/favicon.png',
         },
-      ],
-    ],
-    experiments: {
-      typedRoutes: true,
+        plugins: [
+            'expo-router',
+            [
+                'expo-splash-screen',
+                {
+                    image: './assets/images/splash.png',
+                    imageWidth: 800,
+                    resizeMode: 'cover',
+                    backgroundColor: '#a435f1',
+                },
+            ],
+            [
+                'expo-dev-client',
+                {
+                    launchMode: 'most-recent',
+                },
+            ],
+            [
+                '@sentry/react-native/expo',
+                {
+                    organization: 'calindra',
+                    project: 'cartesi-hot-wallet',
+                    url: 'https://sentry.io/',
+                },
+            ],
+        ],
+        experiments: {
+            typedRoutes: true,
+        },
+        extra: {
+            router: {
+                origin: false,
+            },
+            eas: {
+                projectId: 'bd0f0df3-44d7-411b-8ccd-9a1345bb32da',
+            },
+        },
+        cli: {
+            appVersionSource: 'remote',
+        },
     },
-    extra: {
-      router: {
-        origin: false,
-      },
-      eas: {
-        projectId: 'bd0f0df3-44d7-411b-8ccd-9a1345bb32da',
-      },
-    },
-    cli: {
-      appVersionSource: 'remote',
-    },
-  },
-}
+};
